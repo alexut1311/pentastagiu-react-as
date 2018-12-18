f2c1b6c94029bd6c8d8a7c2520ceb6f06b78b26e
Serban Alexandru <alexut1311@gmail.com>
2018-12-18 17:32:44 +0200

    Am adaugat tema

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-function tick(){
-    const date=new Date().toLocaleString();
-    const component=(
-        <div>
-            <div>
-                Continut static
-            </div>
-            <div>
-                {date}
-            </div>
-        </div>
-    );
+function writeSomethingRandom() {
+    var letters = 'abcdefghijklmnopqrstuvwxyz';
+    var word = ''
+    for (var i = 0; i < Math.floor(Math.random() * 16); i++)
+        word += letters[Math.floor(Math.random() * 16)]
+    word += ' '
+    if (word.length > 4)
+        return word
+    else
+        return writeSomethingRandom()
+}
+
+function generare() {
+    var word = ''
+    for (var i = 0; i < 2500; i++){
+        word += writeSomethingRandom() + " "
+        if (i === Math.floor(Math.random() * 1337))
+            word+=' easter egg si inca ceva '
+        }
+    return word
+}
+alert('Epilepsy alert , don`t enter if you have problems with flashing colors ')
+
+function tick() {
+
+    const element = React.createElement('div', {
+        id: 'div1',
+        style: {
+            backgroundColor: getRandomColor(),
+            height: '100%'
+        }
+    }, 'Bun venit  ' + generare())
 
 
-ReactDOM.render(component, document.getElementById("root"))}
+    ReactDOM.render(element, document.getElementById("root"))
+}
 
-setInterval(tick,1000)
+setInterval(tick, 1)
